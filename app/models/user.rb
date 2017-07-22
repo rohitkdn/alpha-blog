4d1432e421638c8cad9372911c8e8b1dd04330ef
Rohit Kadiyan <rohitkdn@hotmail.com>
2017-07-22 12:23:44 +0530

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: true
-  validates :fullname, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  validates :fullname, presence: true, length: {minimum: 3, maximum: 30}
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+             uniqueness: {case_sensitive: false},
+             length: {minimum: 3, maximum: 30}, format: {with: VALID_EMAIL_REGEX }
 end
