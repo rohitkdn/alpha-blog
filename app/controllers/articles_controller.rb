2bdb083f2bb4859ce8735525241c8e9daa6aae61
Rohit Kadiyan <rohitkdn@hotmail.com>
2017-07-23 15:44:19 +0530

    Complete autentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:update, :show, :edit, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 1)
+    @articles = Article.paginate(page: params[:page], per_page: 3)
 
   end
 
@@ -50,5 +52,12 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can edit or delete only your own articles"
+        redirect_to root_path
+      end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+
+
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
   end
@@ -18,12 +22,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
+        if @user.update(user_params)
       flash[:success] = "Profile successfully updated."
       redirect_to articles_path
     else
@@ -32,7 +34,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
   end
 
@@ -41,5 +42,14 @@ class UsersController < ApplicationController
       params.require(:user).permit(:username, :fullname, :email, :password)
     end
 
+    def set_user
+      @user = User.find(params[:id])
+    end
 
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can edit only your profile"
+        redirect_to user_path(@user)
+      end
+    end
 end
