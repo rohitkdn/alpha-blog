b814c395e6346fffab59e56c7df8196ee71c960f
Rohit Kadiyan <rohitkdn@hotmail.com>
2017-07-26 11:51:07 +0530

    Associations and authentication completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
 
   def index
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-    debugger
   end
 
   def edit
@@ -40,6 +39,15 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 1)
   end
 
+  def destroy
+    @user.destroy
+    if @user == current_user
+      session[:user_id] = nil
+    end
+    flash[:success] = "User successfully deleted"
+    redirect_to root_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :fullname, :email, :password)
@@ -51,8 +59,8 @@ class UsersController < ApplicationController
 
     def require_same_user
       if current_user != @user and !current_user.admin?
-        flash[:danger] = "You can edit only your profile"
-        redirect_to user_path(@user)
+        flash[:danger] = "You can edit or delete only your profile"
+        redirect_to root_path
       end
     end
 end
